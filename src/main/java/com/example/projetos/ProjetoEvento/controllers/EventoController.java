b363119a1dcb63c731ee697fc7316e8f0c10e114
unknown <cleber_batista2010@hotmail.com>
2018-07-18 23:06:33 -0300

    Criando listagem de eventos

--- src/main/java/com/example/projetos/ProjetoEvento/controllers/EventoController.java
+++ src/main/java/com/example/projetos/ProjetoEvento/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.projetos.ProjetoEvento.models.Evento;
 import com.example.projetos.ProjetoEvento.repository.EventoRepository;
@@ -28,5 +29,17 @@ public class EventoController {
 		return "redirect:/cadastrarEvento";
 
 	}
+	
+	@RequestMapping("/eventos")
+	public  ModelAndView listaEventos() {
+		
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = eventorepository.findAll();
+		mv.addObject("eventos",eventos);
+		return mv;
+		
+	}
+	
+	
 
 }
